6eb4d81856f7d044e2a8deb144cb1edc433653e2
Jose Paz <jpaz@li-d6b0314c-2914-11b2-a85c-d1dcaa9a0adc.ibm.com>
2021-03-31 10:55:45 -0300

    agregado tfvars

--- provider.tf
+++ provider.tf
@@ -7,10 +7,6 @@ terraform {
     }
 }
 
-variable "ibmcloud_api_key" {
-    description = "API Key de la nube de IBM, generada en Manage (IAM) -> API Keys"
-}
-
 # Para llamar a las variables colocamos "var" seguido del nombre que le colocamos a ese bloque
 provider "ibm" {
     ibmcloud_api_key = var.ibmcloud_api_key
---
+++ variables.tfvars
@@ -0,0 +1,3 @@
+variable "ibmcloud_api_key" {
+    description = "API Key de la nube de IBM, generada en Manage (IAM) -> API Keys"
+}
