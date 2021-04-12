043960e53e301f678cd2e4d1985eb1650733af72
Jose Paz <jpaz@li-d6b0314c-2914-11b2-a85c-d1dcaa9a0adc.ibm.com>
2021-04-12 12:53:56 -0300

    Cambio en depends_on

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,17 +1,17 @@
-# resource "ibm_container_cluster" "kubernetes_cluster" {
-#   name            = "terraform-cluster-test"
-#   datacenter      = "dal10"
-#   machine_type    = "free"
-#   hardware        = "shared"
+resource "ibm_container_cluster" "kubernetes_cluster" {
+  name            = "terraform-cluster-test"
+  datacenter      = "dal10"
+  machine_type    = "free"
+  hardware        = "shared"
 
-#   no_subnet        = true
+  no_subnet        = true
 
-#   default_pool_size = 1
-# }
+  default_pool_size = 1
+}
 
 data "ibm_container_cluster_config" "cluster_config" {
-  # cluster_name_id   = ibm_container_cluster.kubernetes_cluster.id
-  cluster_name_id   = var.cluster_id
+  cluster_name_id   = ibm_container_cluster.kubernetes_cluster.id
+  
   # Usado para tener nuestra sesión de kubectl y autenticación con calico
   admin           = "true"
   network         = "true"
--- provider.tf
+++ provider.tf
@@ -11,10 +11,6 @@ variable "ibmcloud_api_key" {
   description = "API Key de la nube de IBM, generada en Manage (IAM) -> API Keys"
 }
 
-variable "cluster_id" {
-  description = "ID del cluster"
-}
-
 # Para llamar a las variables colocamos "var" seguido del nombre que le colocamos a ese bloque
 provider "ibm" {
     ibmcloud_api_key = var.ibmcloud_api_key
